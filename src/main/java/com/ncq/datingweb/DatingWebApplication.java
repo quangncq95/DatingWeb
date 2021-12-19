6f15ec728918363759a4a9ee4967b07abae567c6
QuangNC <nguyencaoquang.95@gmail.com>
2021-12-20 01:31:37 +0700

    QuangNC: create "Hello World" spring boot

---
+++ src/main/java/com/ncq/datingweb/DatingWebApplication.java
@@ -0,0 +1,13 @@
+package com.ncq.datingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatingWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatingWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ncq/datingweb/DatingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncq.datingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
